8567f1db3d1455a39b0ba6b2f4ff7e27bb1536ea
acbuie <aidancbuie@gmail.com>
2021-08-26 15:56:18 -0600

    fix: update __init__.py
    Remove __version__ and import functions from main.py

--- sandrose/__init__.py
+++ sandrose/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+from .main import calc_drift_potential, calc_time_percentage, wind_log_law
+
+__all__ = [
+    "calc_drift_potential",
+    "calc_time_percentage",
+    "wind_log_law",
+]
