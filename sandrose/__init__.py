888518ea63f5c79b188d5dc8147a955d84bb3621
acbuie <aidancbuie@gmail.com>
2021-10-08 12:17:33 -0600

    chore: update __init__.py
    Add all modules to auto-import

--- sandrose/__init__.py
+++ sandrose/__init__.py
@@ -15,4 +15,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import sandrose.dp
+from sandrose import bins, conversions, dp, plot
